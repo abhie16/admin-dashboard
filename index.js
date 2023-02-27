ecb5828d39a941de46bf6e0b836fe50e6549c0cc
Soham Sarkar <sohamsarkar59@gmail.com>
2023-02-27 23:58:57 +0530

    handling logo

--- index.js
+++ index.js
@@ -2,38 +2,47 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
+const logoImg = document.querySelector(".logoImg");
+
+//handling logo
+logoImg.setAttribute("src", window.location.href + "/images/logo.png");
 
 // show sidebar
-menuBtn.addEventListener('click', ()=> {
-    sideMenu.style.display= 'block';
-})
-
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
 
 //close sidebar
-closeBtn.addEventListener('click',()=>{
-    sideMenu.style.display='none';
-} )
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
 
 //change theme
-themeToggler.addEventListener('click',() => {
-    document.body.classList.toggle('dark-theme-variables');
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
 
-    themeToggler.querySelector('span:first-child').classList.toggle('active');
-    themeToggler.querySelector('span:last-child').classList.toggle('active');
-})
+  themeToggler.querySelector("span:first-child").classList.toggle("active");
+  themeToggler.querySelector("span:last-child").classList.toggle("active");
+});
 
-// fill orders in table 
+// fill orders in table
 
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
                         <td>${order.productName}</td>
                         <td>${order.productNumber}</td>
                         <td>${order.paymentStatus}</td>
-                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'primary' }">${order.shipping}</td>
+                        <td class="${
+                          order.shipping === "Declined"
+                            ? "danger"
+                            : order.shipping === "Pending"
+                            ? "warning"
+                            : "primary"
+                        }">${order.shipping}</td>
                         <td class="primary">Details</td>
                     `;
-    
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
-})
+
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
